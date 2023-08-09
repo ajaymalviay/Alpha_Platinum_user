86dbd454f473562931d841867aa703832eb13193
AjayMalviya <ajay.malpahwizz@gmail.com>
2023-08-09 17:36:27 +0100

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,19 @@
+final String appName = 'Alpha Platinum';
+
+final String packageName = 'com.alphaPlatinum.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.alphaPlatinum.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.alphaPlatinum.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+final String baseUrl = 'https://alphaplatinum.productsalphawizz.com/app/v1/api/';
+final String imageUrl = 'https://alphaplatinum.productsalphawizz.com/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
